425e1e5a8d76bbb12caa03ccb8404e4e2ba57887
jonasbn <jonasbn@gmail.com>
2022-01-24 20:41:26 +0100

    Cleaned the code and added support for reading from STDIN

--- punyencode.go
+++ punyencode.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 
 	"golang.org/x/net/idna"
 )
 
 func main() {
-	argStr := os.Args[1] // we only take a single parameter and
+	argsWithoutProg := os.Args[1:]
 
-	asciiStr, err := idna.ToASCII(argStr)
+	var regularString string
+
+	if len(argsWithoutProg) <= 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+
+		for scanner.Scan() {
+			regularString = scanner.Text()
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		regularString = os.Args[1] // we only take a single parameter, the string to decode
+	}
+
+	punycodeString, err := idna.ToASCII(regularString)
 
 	if err == nil {
-		fmt.Printf("%s\n", asciiStr)
+		fmt.Printf("%s\n", punycodeString)
 	}
 }
